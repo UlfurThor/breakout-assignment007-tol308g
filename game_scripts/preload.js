61d18e83abaad8b1c20510a9e05b258add70e487
UlfurThor <ulfurthor@gmail.com>
2018-10-13 13:56:49 +0000

    game rebuild restarted

--- game_scripts/preload.js
+++ game_scripts/preload.js
@@ -3,7 +3,7 @@
 var g_sprites = [];
 var g_shipImage = "";
 //g_shipImage.src = "https://notendur.hi.is/~uth16/games_design/rocketshiptothestarsdotjeypeg.png";
-var shipPath = "https://notendur.hi.is/~pk/308G/images/ship.png";
+var shipPath = "img.jpg";
 function preloadStuff_thenCall(completionCallback) {
     var g_shipImage = new Image();
     g_shipImage.src = shipPath;
